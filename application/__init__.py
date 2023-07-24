47aea8e487e049f0c6790c3223aa49d016708455
Lyuda Kim <lyudarkim@gmail.com>
2023-07-24 13:41:51 -0700

    Set up connection to database

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from flask_pymongo import PyMongo
-import os
-from dotenv import load_dotenv
 from flask_cors import CORS
 from application.utils.database import setup_db_connection
-
-load_dotenv()
 
 
 def create_app(test_config=None):
@@ -13,10 +8,7 @@ def create_app(test_config=None):
     CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
 
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-
-    # Call the setup_db_connection function to set up the database connection
+    # Initialize the connection to MongoDB using Flask-PyMongo
     mongodb_client = setup_db_connection(app)
 
     # Register Blueprints 
--- application/utils/database.py
+++ application/utils/database.py
@@ -1,11 +1,31 @@
+import os
+import certifi
+import urllib.parse
+from dotenv import load_dotenv
 from flask_pymongo import PyMongo
 
 
-# Create the PyMongo instance for database connection
-mongodb_client = PyMongo()
+load_dotenv()
+
+# Encode the path to the certificate we get from certifi.where()
+cert_path = urllib.parse.quote_plus(certifi.where())
+
+MONGO_URI = os.environ.get('MONGO_URI')
+# Append the encoded tlsCAFile param to the MongoDB URI
+# Without it, the 'pymongo [SSL: CERTIFICATE_VERIFY_FAILED]' error occurs
+MONGO_URI += f"?tlsCAFile={cert_path}"
+
+# Create an instance of Flask-PyMongo which we will initialize later 
+pymongo = PyMongo()
+
 
 def setup_db_connection(app):
-    # Set up MongoDB with the Flask app
-    mongodb_client.init_app(app)
+    """
+    This function sets up the connection to MongoDB using Flask-PyMongo
+    """
+    app.config['MONGO_URI'] = MONGO_URI
+    pymongo.init_app(app)
 
-    return mongodb_client  # Return the mongodb_client to the caller (create_app)
+    return pymongo  
+
+
