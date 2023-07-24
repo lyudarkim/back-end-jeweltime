f617fe081e5fcb4e95d7d08c043ac1adc79cec55
Lyuda Kim <lyudarkim@gmail.com>
2023-07-24 13:49:09 -0700

    Rename global variable to the one used in .env file

--- application/utils/database.py
+++ application/utils/database.py
@@ -10,10 +10,14 @@ load_dotenv()
 # Encode the path to the certificate we get from certifi.where()
 cert_path = urllib.parse.quote_plus(certifi.where())
 
-MONGO_URI = os.environ.get('MONGO_URI')
+MONGODB_URI = os.environ.get('MONGODB_URI')
+
+if not MONGODB_URI:
+    raise ValueError("MONGODB_URI is not set in the environment or .env file")
+
 # Append the encoded tlsCAFile param to the MongoDB URI
 # Without it, the 'pymongo [SSL: CERTIFICATE_VERIFY_FAILED]' error occurs
-MONGO_URI += f"?tlsCAFile={cert_path}"
+MONGODB_URI += f"?tlsCAFile={cert_path}"
 
 # Create an instance of Flask-PyMongo which we will initialize later 
 pymongo = PyMongo()
@@ -23,7 +27,7 @@ def setup_db_connection(app):
     """
     This function sets up the connection to MongoDB using Flask-PyMongo
     """
-    app.config['MONGO_URI'] = MONGO_URI
+    app.config['MONGO_URI'] = MONGODB_URI
     pymongo.init_app(app)
 
     return pymongo  
