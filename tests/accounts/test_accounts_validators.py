c0134fe1950c968e6f4f4cf58ad97692eea969a0
Lyuda Kim <lyudarkim@gmail.com>
2023-07-25 14:59:25 -0700

    Implement two tests for last_name field in AccountSchema class

--- tests/accounts/test_accounts_validators.py
+++ tests/accounts/test_accounts_validators.py
@@ -3,10 +3,11 @@ from marshmallow import ValidationError
 from application.modules.accounts.validators import AccountSchema
 
 
+# Tests for first_name
 def test_first_name_is_not_empty_string():     
     schema = AccountSchema()
     data = {
-        "first_name": "",   # Empty first_name
+        "first_name": "",  
         "last_name": "Didion",
         "email": "juniper@didion.com",
         "zipcode": "98104"
@@ -21,7 +22,6 @@ def test_first_name_is_not_empty_string():
 def test_first_name_field_is_required():
     schema = AccountSchema()
     data = {
-        # Omitting "first_name" to trigger the "required" validation
         "last_name": "Didion",
         "email": "juniper@didion.com",
         "zipcode": "98104"
@@ -33,3 +33,31 @@ def test_first_name_field_is_required():
     assert excinfo.value.messages['first_name'] == ["First name is required."]
 
 
+# Tests for last_name
+def test_last_name_is_not_empty_string():     
+    schema = AccountSchema()
+    data = {
+        "first_name": "Juniper",
+        "last_name": "",  
+        "email": "juniper@didion.com",
+        "zipcode": "98104"
+    }
+    
+    with pytest.raises(ValidationError) as excinfo:
+        schema.load(data)
+
+    assert excinfo.value.messages['last_name'] == ["Field cannot be empty or consist solely of whitespace."]
+
+
+def test_last_name_field_is_required():
+    schema = AccountSchema()
+    data = {
+        "first_name": "Juniper",
+        "email": "juniper@didion.com",
+        "zipcode": "98104"
+    }
+    
+    with pytest.raises(ValidationError) as excinfo:
+        schema.load(data)
+
+    assert excinfo.value.messages['last_name'] == ["Last name is required."]
