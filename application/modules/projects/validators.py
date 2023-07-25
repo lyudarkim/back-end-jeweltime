5e0843eee8e7f43112b55822e2942594c6f60c90
Lyuda Kim <lyudarkim@gmail.com>
2023-07-24 21:25:54 -0700

    Add field-level validations to ProjectSchema and refine error messages

--- application/modules/projects/validators.py
+++ application/modules/projects/validators.py
@@ -1,40 +1,39 @@
 from marshmallow import fields, Length, Schema, ValidationError
-
-custom_errors = {
-    "required": "{field_name} is required.",
-    "invalid_length": "{field_name} exceeds the maximum length."
-}
+from application.utils.helpers import validate_not_only_whitespace
 
 
 class ProjectSchema(Schema):
-    # Using ObjectId which is a hex string
+    # Using ObjectId which is a hex string.
     account_id = fields.String(
         required=True,
-        error_messages={
-            "required": custom_errors["required"].format(field_name="Account ID")
-        }
+        error_messages={"required": "Account ID is required."}
     )
+    
     project_id = fields.String(dump_only=True)
+    
     project_name = fields.Str(
         required=True,
-        error_messages={
-            "required": custom_errors["required"].format(field_name="Project name")
-        }
+        validate=[
+            Length(min=1, error="Field cannot be empty."),
+            validate_not_only_whitespace
+        ],
+        error_messages={"required": "Project name is required."}
     )
+    
     description = fields.Str(
-        required=True, 
-        validate=Length(max=300),
-        error_messages={
-            "required": custom_errors["required"].format(field_name="Description"),
-            "invalid": custom_errors["invalid_length"].format(field_name="Description")
-        }
+        required=True,
+        validate=[
+            Length(max=300, error="Description exceeds the maximum length."),
+            validate_not_only_whitespace
+        ],
+        error_messages={"required": "Description is required."}
     )
+    
     started_at = fields.DateTime(
         required=True,
-        error_messages={
-            "required": custom_errors["required"].format(field_name="Start date")
-        }
+        error_messages={"required": "Start date is required."}
     )
+    
     completed_at = fields.DateTime(allow_none=True)
     hours_spent = fields.Float(allow_none=True)
     materials_cost = fields.Float(allow_none=True)
@@ -43,3 +42,4 @@ class ProjectSchema(Schema):
     gemstones = fields.List(fields.Str(), default=[])
     shape = fields.Str(allow_none=True)
     jewelry_type = fields.Str(allow_none=True)
+
