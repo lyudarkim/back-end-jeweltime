dbded1c8bc8841c7dc5e6987bd285aea0afbc7ab
Lyuda Kim <lyudarkim@gmail.com>
2023-07-25 12:57:23 -0700

    Refactor helper function to validate that field isn't empty or whitespace

--- application/utils/helpers.py
+++ application/utils/helpers.py
@@ -1,7 +1,7 @@
 from marshmallow import ValidationError
 
 
-def validate_not_only_whitespace(data):
-    """Ensure the provided data isn't just whitespace."""
-    if data.strip() == "":
-        raise ValidationError("Field cannot consist solely of whitespace.")
+def validate_not_empty_or_whitespace(data):
+    """Ensure the provided data isn't just whitespace or empty."""
+    if not data or data.strip() == "":
+        raise ValidationError("Field cannot be empty or consist solely of whitespace.")
