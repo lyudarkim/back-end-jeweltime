a5cd6d51ea8e47d9d4c636e5cab24f82679e3327
Lyuda Kim <lyudarkim@gmail.com>
2023-07-25 15:27:25 -0700

    Implement five tests for zipcode field in AccountSchema class

--- tests/accounts/test_accounts_validators.py
+++ tests/accounts/test_accounts_validators.py
@@ -91,3 +91,78 @@ def test_email_field_is_required():
         schema.load(data)
 
     assert excinfo.value.messages['email'] == ["Email is required."]
+
+
+# Tests for zipcode
+def test_zipcode_is_not_empty_string():     
+    schema = AccountSchema()
+    data = {
+        "first_name": "Juniper",
+        "last_name": "Didion",
+        "email": "juniper@didion.com",
+        "zipcode": ""  
+    }
+    
+    with pytest.raises(ValidationError) as excinfo:
+        schema.load(data)
+
+    assert "Field cannot be empty or consist solely of whitespace." in excinfo.value.messages['zipcode']
+
+
+def test_zipcode_is_not_whitespace_only():     
+    schema = AccountSchema()
+    data = {
+        "first_name": "Juniper",
+        "last_name": "Didion",
+        "email": "juniper@didion.com",
+        "zipcode": "     "  
+    }
+    
+    with pytest.raises(ValidationError) as excinfo:
+        schema.load(data)
+
+    assert excinfo.value.messages['zipcode'] == ["Field cannot be empty or consist solely of whitespace."]
+
+
+def test_zipcode_field_is_required():
+    schema = AccountSchema()
+    data = {
+        "first_name": "Juniper",
+        "last_name": "Didion",
+        "email": "juniper@didion.com",
+    }
+    
+    with pytest.raises(ValidationError) as excinfo:
+        schema.load(data)
+
+    assert excinfo.value.messages['zipcode'] == ["Zipcode is required."]
+
+
+def test_zipcode_does_not_exceed_max_length():
+    schema = AccountSchema()
+    data = {
+        "first_name": "Juniper",
+        "last_name": "Didion",
+        "email": "juniper@didion.com",
+        "zipcode": "12345678901"  
+    }
+    
+    with pytest.raises(ValidationError) as excinfo:
+        schema.load(data)
+
+    assert excinfo.value.messages['zipcode'] == ["Length must be between 3 and 10."]
+
+
+def test_zipcode_reaches_minimum_length():
+    schema = AccountSchema()
+    data = {
+        "first_name": "Juniper",
+        "last_name": "Didion",
+        "email": "juniper@didion.com",
+        "zipcode": "12"  
+    }
+    
+    with pytest.raises(ValidationError) as excinfo:
+        schema.load(data)
+
+    assert excinfo.value.messages['zipcode'] == ["Length must be between 3 and 10."]
