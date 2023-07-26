3a1f56d6c42d2ed647887b523004406ed963f142
Lyuda Kim <lyudarkim@gmail.com>
2023-07-25 20:41:32 -0700

    Implement 4 tests for description field in ProjectSchema class

--- tests/projects/test_projects_validators.py
+++ tests/projects/test_projects_validators.py
@@ -24,3 +24,36 @@ def test_project_name_field_is_required(base_project_data):
     
     errors = validate_project(data)
     assert errors['project_name'] == ["Project name is required."]
+
+
+# Tests for description
+def test_description_is_not_empty_string(base_project_data):
+    data = base_project_data.copy()
+    data["description"] = ""
+    
+    errors = validate_project(data)
+    assert errors['description'] == ["Field cannot be empty or consist solely of whitespace."]
+
+
+def test_description_is_not_whitespace_only(base_project_data):
+    data = base_project_data.copy()
+    data["description"] = "     "
+    
+    errors = validate_project(data)
+    assert errors['description'] == ["Field cannot be empty or consist solely of whitespace."]
+
+
+def test_description_field_is_required(base_project_data):
+    data = base_project_data.copy()
+    del data["description"]
+    
+    errors = validate_project(data)
+    assert errors['description'] == ["Description is required."]
+
+
+def test_description_does_not_exceed_max_length(base_project_data):
+    data = base_project_data.copy()
+    data["description"] = "a" * 301  # One character more than the maximum
+    
+    errors = validate_project(data)
+    assert errors["description"] == ["Description exceeds the maximum length."]
