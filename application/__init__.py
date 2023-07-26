70d38a1177fea5cee335b538b89966376068fcc0
Lyuda Kim <lyudarkim@gmail.com>
2023-07-26 15:10:14 -0700

    Streamline testing config

--- application/__init__.py
+++ application/__init__.py
@@ -2,19 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from application.utils.database import setup_db_connection
 
-
-def create_app(test_config=None):
+# Remove testing=True argument when in production
+def create_app(testing=True):
     app = Flask(__name__)
     CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
 
-    # Use test_config to determine which database to connect to
-    testing = False
-    if test_config:
-        testing = True
-
     # Initialize the connection to MongoDB using Flask-PyMongo
-    mongodb_client = setup_db_connection(app, testing)
+    setup_db_connection(app, testing)
 
     # Register Blueprints 
     from .modules.accounts.routes import accounts_bp
