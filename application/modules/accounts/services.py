5813bc0164cb2774288399ce54d3af34c6966153
Lyuda Kim <lyudarkim@gmail.com>
2023-07-27 11:05:45 -0700

    Implement get_account function and its test

--- application/modules/accounts/services.py
+++ application/modules/accounts/services.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from application.utils.database import pymongo
 
 
@@ -5,3 +6,7 @@ def create_account(data):
     """Insert new account into the database."""
     return pymongo.db.accounts.insert_one(data).inserted_id
 
+
+def get_account(account_id):
+    """Get a specific account using its ID."""
+    return pymongo.db.accounts.find_one({"_id": ObjectId(account_id)})
--- tests/accounts/test_accounts_services.py
+++ tests/accounts/test_accounts_services.py
@@ -1,4 +1,4 @@
-from application.modules.accounts.services import create_account
+from application.modules.accounts.services import create_account, get_account
 from application.utils.database import pymongo
 
 
@@ -9,3 +9,12 @@ def test_create_account(app, base_account_data):
         
         retrieved_account = pymongo.db.accounts.find_one({"_id": account_id})
         assert retrieved_account["first_name"] == base_account_data["first_name"]
+
+
+def test_get_account(app, base_account_data):
+    with app.app_context():
+        account_id = create_account(base_account_data)
+        account = get_account(str(account_id))
+        
+        assert account
+        assert account["_id"] == account_id
