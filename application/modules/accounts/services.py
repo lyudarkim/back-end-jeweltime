839ca5136b81c4d0282ebc5a98dff3954f76ef00
Lyuda Kim <lyudarkim@gmail.com>
2023-07-27 11:52:00 -0700

    Rename service functions to make their purpose clear

--- application/modules/accounts/services.py
+++ application/modules/accounts/services.py
@@ -2,17 +2,17 @@ from bson import ObjectId
 from application.utils.database import pymongo
 
 
-def create_account(data):
+def service_create_account(data):
     """This function inserts a new account into the database."""
     return pymongo.db.accounts.insert_one(data).inserted_id
 
 
-def get_account(account_id):
+def service_get_account(account_id):
     """This function retrieves a specific account using its ID."""
     return pymongo.db.accounts.find_one({"_id": ObjectId(account_id)})
 
 
-def update_account(account_id, data):
+def service_update_account(account_id, data):
     """
     This function updates an account in the database using its ID. 
     It returns the count of updated records (should be 1 if the operation was successful).
@@ -22,7 +22,7 @@ def update_account(account_id, data):
     return result.modified_count
     
 
-def delete_account(account_id):
+def service_delete_account(account_id):
     """
     This function deletes an account from the database using its ID.
     It returns the count of deleted records (should be 1 if the operation was successful).
