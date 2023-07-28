3bb67ffa97e70ddb2ddf76d0e344a31382a421af
Lyuda Kim <lyudarkim@gmail.com>
2023-07-27 20:56:16 -0700

    Refactor 2 service functions to associate account_id with a project

--- application/modules/projects/services.py
+++ application/modules/projects/services.py
@@ -2,12 +2,23 @@ from bson import ObjectId
 from application.utils.database import pymongo
 
 
-def service_create_project(data):
-    """This function inserts a new project into the database."""
-    return pymongo.db.projects.insert_one(data).inserted_id
+def service_create_project(data, account_id):
+    """This function inserts a new project into the database and associates it with an account."""
     
+    if not pymongo.db.accounts.find_one({"_id": ObjectId(account_id)}):
+        raise ValueError("Account not found.")
+    
+    # Add key-value pair with the account_id to the project
+    data["account_id"] = ObjectId(account_id)  
 
-def service_get_project(project_id):
-    """This function retrieves a specific project using its ID."""
+    return pymongo.db.projects.insert_one(data).inserted_id
+
+    
+def service_get_project(project_id, account_id):
+    """This function retrieves a project associated with an account using project ID."""
+
+    if not pymongo.db.accounts.find_one({"_id": ObjectId(account_id)}):
+        raise ValueError("Account not found.")
+
     return pymongo.db.projects.find_one({"_id": ObjectId(project_id)})
     
--- tests/projects/test_projects_services.py
+++ tests/projects/test_projects_services.py
@@ -1,4 +1,10 @@
 # Service functions
+from application.modules.accounts.services import (
+    service_create_account, 
+    # service_get_account, 
+    # service_update_account, 
+    # service_delete_account
+)
 from application.modules.projects.services import (
     service_create_project, 
     service_get_project, 
@@ -10,19 +16,26 @@ from application.modules.projects.services import (
 from application.utils.database import pymongo
 
 
-def test_create_project(app, base_project_data):
+def test_create_project(app, base_account_data, base_project_data):
     with app.app_context():
-        project_id = service_create_project(base_project_data)
+        account_id = service_create_account(base_account_data)
+        assert account_id
+
+        project_id = service_create_project(base_project_data, str(account_id))
         assert project_id
         
         retrieved_project = pymongo.db.projects.find_one({"_id": project_id})
         assert retrieved_project["project_name"] == base_project_data["project_name"]
+        assert retrieved_project["account_id"] == account_id
 
 
-def test_get_project(app, base_project_data):
+def test_get_project(app, base_account_data, base_project_data):
     with app.app_context():
-        project_id = service_create_project(base_project_data)
-        project = service_get_project(str(project_id))
+        account_id = service_create_account(base_account_data)
+        project_id = service_create_project(base_project_data, str(account_id))
+
+        project = service_get_project(str(project_id), str(account_id))
         
         assert project
         assert project["_id"] == project_id
+        assert project["account_id"] == account_id
