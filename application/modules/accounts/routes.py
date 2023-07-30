4b49b8df29517afc6956f19b299d20794f5520ba
Lyuda Kim <lyudarkim@gmail.com>
2023-07-29 20:54:20 -0700

    Implement @handle_errors decorator for account routes
    - Refactor account routes to use decorator
    - Account routes are now more DRY

--- application/modules/accounts/routes.py
+++ application/modules/accounts/routes.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, jsonify, request, abort
-# from bson import ObjectId  
-from bson.errors import InvalidId
 from application.modules.accounts.services import (
     service_create_account, 
     service_get_account, 
@@ -8,88 +6,66 @@ from application.modules.accounts.services import (
     service_delete_account
 )
 from application.modules.accounts.validators import validate_account
-from pymongo.errors import ConnectionFailure
+from application.utils.helpers import handle_errors
 
 
 accounts_bp = Blueprint("accounts", __name__, url_prefix="/accounts")
 
 
 @accounts_bp.route("", methods=['POST'])
+@handle_errors
 def create_account():
-    try:
-        data = request.json
-        errors = validate_account(data, partial=False)
-        
-        if errors:
-            return jsonify(errors), 400
-        
-        # Fetch the whole account
-        new_account = service_create_account(data)
-        
-        return jsonify(new_account), 201
+    data = request.json
+    errors = validate_account(data, partial=False)
     
-    except ConnectionFailure:
-        return jsonify({
-            "error": "Database connection failed"
-        }), 500
+    if errors:
+        return jsonify(errors), 400
+    
+    new_account = service_create_account(data)
+    
+    return jsonify(new_account), 201
 
 
 @accounts_bp.route("/<accountId>", methods=['GET'])
+@handle_errors
 def get_account(accountId):
-    try:
-        account = service_get_account(accountId)
-        if not account:
-            abort(404, description="Account not found")
-        
-        return jsonify(account), 200
+    account = service_get_account(accountId)
 
-    # If 'accountId' is not a valid BSON ObjectId
-    except InvalidId:
-        return jsonify({
-            "error": "Invalid account ID format"
-        }), 400
+    if not account:
+        abort(404, description="Account not found")
+    
+    return jsonify(account), 200
 
 
 @accounts_bp.route("/<accountId>", methods=['PUT', 'PATCH'])
+@handle_errors
 def update_account(accountId):
-    try: 
-        data = request.json
-        errors = validate_account(data, partial=True)
+    data = request.json
+    errors = validate_account(data, partial=True)
 
-        if errors:
-            return jsonify(errors), 400
+    if errors:
+        return jsonify(errors), 400
 
-        account = service_update_account(accountId, data)
-        
-        if not account:
-            abort(404, description="Account not found or not updated.")
-        
-        return jsonify({
-            "account": account, 
-            "message": "Account updated successfully"
-        })
+    account = service_update_account(accountId, data)
     
-    except InvalidId:
-            return jsonify({"error": "Invalid account ID format"}), 400
-        
-    except ConnectionFailure:
-        return jsonify({"error": "Database connection failed"}), 500
+    if not account:
+        abort(404, description="Account not found or not updated.")
     
+    return jsonify({
+        "account": account, 
+        "message": "Account updated successfully"
+    })
+
 
 @accounts_bp.route("/<accountId>", methods=['DELETE'])
+@handle_errors
 def delete_account(accountId):
-    try:
-        count = service_delete_account(accountId)
-        
-        if count == 0:
-            abort(404, description="Account not found.")
-        
-        return jsonify({"message": "Account deleted successfully"})
+    count = service_delete_account(accountId)
+
+    if count == 0:
+        abort(404, description="Account not found.")
     
-    except InvalidId:
-        return jsonify({"error": "Invalid account ID format"}), 400
-        
-    except ConnectionFailure:
-        return jsonify({"error": "Database connection failed"}), 500
-
-
+    return jsonify({
+        "message": "Account deleted successfully", 
+        "accountId": accountId
+    })
--- application/utils/helpers.py
+++ application/utils/helpers.py
@@ -1,7 +1,60 @@
+from bson.errors import InvalidId
+from flask import jsonify
+from functools import wraps
+import logging
 from marshmallow import ValidationError
+from werkzeug.exceptions import HTTPException  
+from pymongo.errors import ConnectionFailure
 
 
 def validate_not_empty_or_whitespace(data):
-    """Ensure the provided data isn't just whitespace or empty."""
+    """This function ensures that the provided data isn't just whitespace or empty."""
     if not data or data.strip() == "":
         raise ValidationError("Field cannot be empty or consist solely of whitespace.")
+
+
+def handle_errors(function):
+    """
+    A higher-order function used as a decorator to catch and handle exceptions
+    for Flask route functions.
+    Returns appropriate JSON responses for errors, logging unexpected ones.
+
+    Parameters:
+    - function: The function representing a Flask route to enhance with error handling.
+
+    Returns:
+    - A new function that handles errors and wraps around the original Flask route.
+    """
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+
+        # Specific exceptions first
+        except InvalidId:
+            return jsonify({
+                "error": "Invalid account ID format"
+            }), 400
+
+        except ConnectionFailure:
+            return jsonify({
+                "error": "Database connection failed"
+            }), 500
+
+        # Flask's HTTPException (like abort())
+        except HTTPException as e:  
+            return jsonify({
+                "error": e.description
+            }), e.code
+        
+        # General exception for unexpected errors
+        except Exception:
+            # Log the full error to diagnose issues in production
+            logging.error("Unexpected error occurred", exc_info=True)
+
+            # But return only a generic error message to the user
+            return jsonify({
+                "error": "An unexpected error occurred"
+            }), 500
+    
+    return wrapper
