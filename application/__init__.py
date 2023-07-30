7257c66a15365c54fad8ed595432949264be9586
Lyuda Kim <lyudarkim@gmail.com>
2023-07-29 20:57:22 -0700

    Implement configure_logging function in __init__.py
    - Logging important messages in production
    - Logging more messages in development

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
+import logging
 from application.utils.database import setup_db_connection
+
+
+def configure_logging(app):
+    if app.debug:
+        # In development mode, log all messages, including lower-severity ones
+        logging_level = logging.DEBUG 
+    else:
+        # In production, log only important messages for better clarity
+        logging_level = logging.INFO
+
+    # Capture routine operations and above from Flask's internal server (Werkzeug)
+    werkzeug_log_level = logging.INFO  
+
+    # Set the format for displaying logs
+    logging.basicConfig(level=logging_level, 
+                        format='\n%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+
+    # Set Werkzeug log level
+    logging.getLogger('werkzeug').setLevel(werkzeug_log_level)
 
 
 # Remove testing=True argument when in production
@@ -18,5 +38,8 @@ def create_app(testing=True):
 
     app.register_blueprint(accounts_bp)
     app.register_blueprint(projects_bp)
+
+    # Set up logging
+    configure_logging(app)  
 
     return app
