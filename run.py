b22bf21ceb47ebc8a63ff50b9c47eb477a5347f3
Lyuda Kim <lyudarkim@gmail.com>
2023-07-29 21:02:35 -0700

    Configure Flask debug mode via environment variable
    - Use the 'FLASK_DEBUG' environment variable to set debug mode
    - This enhances deployment flexibility

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+import os
 from application import create_app
 
+
+# If the value of 'FLASK_DEBUG' isn't set, use '0' as a default value
+if os.environ.get('FLASK_DEBUG', default='0') == '1':
+    debug_mode = True
+else:
+    debug_mode = False
 
 app = create_app()
 
 if __name__ == "__main__":
-    # Remember to remove debug=True once the app is in production
-    app.run(debug=True)
+    app.run(debug=debug_mode)
