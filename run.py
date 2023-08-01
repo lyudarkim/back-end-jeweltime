cd5d15c50f3758546fedc03db14d5f631d6fe109
Lyuda Kim <lyudarkim@gmail.com>
2023-07-31 21:37:54 -0700

    Add PORT environment variable

--- run.py
+++ run.py
@@ -12,3 +12,10 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=debug_mode)
+
+    # Get the PORT from environment variables, defaulting to 5000 for local development
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=debug_mode, host='0.0.0.0', port=port)
+
+
+
